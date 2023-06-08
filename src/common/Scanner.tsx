8726d45ff4a2981b494edef138f9de9eb9d6285c
Dayo Akinkuowo <hello@madebydayo.co>
2023-06-08 01:10:21 +0100

    Optimized qrcode scanner

--- src/common/Scanner.tsx
+++ src/common/Scanner.tsx
@@ -1,35 +1,54 @@
 import React, { useState, useEffect } from 'react';
-import { StyleSheet, View } from 'react-native';
+import { useIsFocused } from '@react-navigation/native';
+import { StyleSheet, View, PermissionsAndroid } from 'react-native';
 import { Camera } from 'react-native-camera-kit';
 import { useTheme } from './Colors';
 import { ms } from '@utils';
 
-export const Scanner = () => {
+export const Scanner = ({ onDone }: { onDone: (code: string) => void }) => {
   const [hasPermission, setHasPermission] = useState(false);
+  const isFocused = useIsFocused();
   const styles = useStyles();
 
   useEffect(() => {
     (async () => {
       try {
-        const status = await Camera.checkDeviceCameraAuthorizationStatus();
-        console.log('scanner', status);
-      } catch (error) {
-        console.log('scanner error', error);
+        const granted = await PermissionsAndroid.request(
+          PermissionsAndroid.PERMISSIONS.CAMERA,
+          {
+            title: 'RoutePay Camera Permission',
+            message:
+              'RoutePay needs access to your camera ' +
+              'so you can scan QRcodes',
+            buttonNeutral: 'Ask Me Later',
+            buttonNegative: 'Cancel',
+            buttonPositive: 'OK',
+          },
+        );
+        if (granted === PermissionsAndroid.RESULTS.GRANTED) {
+          console.log('You can use the camera');
+          setHasPermission(true);
+        } else {
+          console.log('Camera permission denied');
+        }
+      } catch (err) {
+        console.warn(err);
       }
     })();
   }, []);
 
-  const onFound = (event: any) => {
-    console.log('QR code found', JSON.stringify(event.nativeEvent));
+  const onFound = ({ nativeEvent }: any) => {
+    const { codeStringValue } = nativeEvent;
+    onDone(codeStringValue);
   };
 
-  return (
+  return isFocused && hasPermission ? (
     <View style={styles.scanner}>
       <View style={styles.wrapper}>
         <Camera scanBarcode={true} onReadCode={onFound} style={styles.camera} />
       </View>
     </View>
-  );
+  ) : null;
 };
 
 export const useStyles = () => {
