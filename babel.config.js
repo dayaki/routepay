d1b8349e878d3309c7f5face54b119b928955636
Dayo Akinkuowo <hello@mdebydayo.co>
2023-06-15 17:14:53 +0100

    Make use of react-native-dotenv got env

--- babel.config.js
+++ babel.config.js
@@ -2,6 +2,13 @@ module.exports = {
   presets: ['module:metro-react-native-babel-preset'],
   plugins: [
     [
+      'module:react-native-dotenv',
+      {
+        safe: true,
+        allowUndefined: false,
+      },
+    ],
+    [
       'module-resolver',
       {
         root: ['.'],
---
+++ src/types/env.d.tsx
@@ -0,0 +1,4 @@
+declare module '@env' {
+  export const TERMII_API: string;
+  export const TERMII_SENDER: string;
+}
